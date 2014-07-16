cc5cbb78527705e2ca1b80cfacc924f0c5c96608
celian <celian.garcia@utbm.fr>
2014-07-16 17:56:06 +0200

    working with frenet frames

--- createPath.js
+++ createPath.js
@@ -1,137 +1,57 @@
-var createPath = function (diameterTop, diameterBottom, tessellation, points, scene) {
+var createPath = function (points, radius, tessellation, scene) {
     var subdivisions = points.length;
-    var radiusTop = diameterTop / 2;
-    var radiusBottom = diameterBottom / 2;
     var indices = [];
     var positions = [];
     var normals = [];
     var uvs = [];
 
-    var height = 1;
-    height = height || 1;
-    diameterTop = diameterTop || 0.5;
-    diameterBottom = diameterBottom || 1;
-    tessellation = tessellation || 16;
-    subdivisions = subdivisions || 4;
-    subdivisions = (subdivisions < 2) ? 2 : subdivisions;
+    var subdivisions = points.length - 1;
+    var frames = frenetFrames( points );
+    console.log(frames);
+    var tangents = frames.tangents,
+    fnormals = frames.normals,
+    binormals = frames.binormals;
 
-    var getCircleVector = function (i) {
-        var angle = (i * 2.0 * Math.PI / tessellation);
-        var dx = Math.cos(angle);
-        var dz = Math.sin(angle);
+    var tangent, normal, binormal;
+    for (var j = 0; j <= tessellation; j++ ) {
+        for (var i in points) {
+            normal = fnormals[i];
+            binormal = binormals[i];
+            var v = j / tessellation * 2 * Math.PI;
+            cx = - radius * Math.cos( v );
+            cy = radius * Math.sin( v );
 
-        return new BABYLON.Vector3(dx, 0, dz);
-    };
-
-    var createCylinderCap = function (isTop) {
-        var radius = isTop ? radiusTop : radiusBottom;
-
-        if (radius == 0) {
-            return;
-        }
-        var vbase = positions.length / 3;
-
-        var normal = new BABYLON.Vector3(0, 1, 0);
-        var textureScale = new BABYLON.Vector2(0.5, 0.5);
-
-        if (!isTop) {
-            normal = normal.scale(-1);
-            textureScale.x = -textureScale.x;
-        }
-
-        // Positions, normals & uvs
-        for (i = 0; i < tessellation; i++) {
-            var circleVector = getCircleVector(i);
-            var position = circleVector.scale(radius).add(normal.scale(height/2));
-            var textureCoordinate = new BABYLON.Vector2(circleVector.x * textureScale.x + 0.5, circleVector.z * textureScale.y + 0.5);
-
-            positions.push(position.x, position.y, position.z);
-            normals.push(normal.x, normal.y, normal.z);
-            uvs.push(textureCoordinate.x, textureCoordinate.y);
-        }
-
-        // Indices
-        for (var i = 0; i < tessellation - 2; i++) {
-            if (!isTop) {
-                indices.push(vbase);
-                indices.push(vbase + (i + 2) % tessellation);
-                indices.push(vbase + (i + 1) % tessellation);
-            } else {
-                indices.push(vbase);
-                indices.push(vbase + (i + 1) % tessellation);
-                indices.push(vbase + (i + 2) % tessellation);
-            }
-        }
-    };
-
-    var base = new BABYLON.Vector3(0, -1, 0).scale(height/2);
-    var offset = new BABYLON.Vector3(0, 1, 0).scale(height/(subdivisions-1));
-
-    var stride = tessellation + 1;
-
-    // Positions, normals & uvs
-    for (var i = 0; i <= tessellation; i++) {
-        var normal = getCircleVector(i);
-        var textureCoordinate = new BABYLON.Vector2(i / tessellation, 0);
-        var position, radius = radiusBottom;
-        
-        for (var s = 0; s < subdivisions; s++) {
-            if (s == 0) {
-                var AC = points[1].subtract(points[0]);
-                var angle = Math.PI/2 - Math.cos(new BABYLON.Vector3(points[1].x, 0, points[1].z).length()/AC.length());
-                //~ if (i == 0) {
-                    displayVector (normal, "red", scene);
-                    displayVector (AC, "blue", scene);
-                    var axis = BABYLON.Vector3.Cross(AC,new BABYLON.Vector3(points[1].x, 0, points[1].z));
-                    displayAxis (points[s], axis, "blue", scene);
-                    var transformMatrix = BABYLON.Matrix.RotationAxis(axis, angle);
-                    var place = BABYLON.Vector3.TransformNormal(normal, transformMatrix);
-                    position = place.scale(radius);
-                    position.addInPlace(points[s]);
-                    displayVector (place, "green", scene);
-                //~ } else {
-                    //~ position = normal.scale(radius);
-                    //~ position.addInPlace(points[s]);
-                //~ }
-                //~ var axis = BABYLON.Vector3.Cross(AC, new BABYLON.Vector3(points[1].x, 0, points[1].z));
-                //~ console.log(points[1]);
-                //~ 
-                //~ 
-                
-            }
-            else {
-                position = normal.scale(radius);
-                position.addInPlace(points[s]);
-            }
-
+            var position = points[i].add((normal.scale(cx)).add(binormal.scale(cy)));
+            var sphere = new BABYLON.Mesh.CreateSphere("name", 10, 0.1, scene);
+            positions.push(
+                position.x,
+                position.y,
+                position.z
+            );
             
-            textureCoordinate.y += 1/(subdivisions -1);
-            radius += (radiusTop - radiusBottom)/(subdivisions - 1);
-
-            // Push in arrays
-            positions.push(position.x, position.y, position.z);
-            normals.push(normal.x, normal.y, normal.z);
-            uvs.push(textureCoordinate.x, textureCoordinate.y);
+            var newNormal = (normal.scale(cx)).add(binormal.scale(cy));
+            displayAxis(position, newNormal, "blue", scene);
+            console.log(normal);
+            normals.push(
+                normal.x,
+                normal.y,
+                normal.z
+            )
         }
     }
 
-    // Indices 
+    var stride = tessellation + 1;
     for (var s = 0; s < subdivisions-1; s++) {
         for (var i = 0; i <= tessellation; i++) {
             indices.push(i * subdivisions + s);// 0
             indices.push((i * subdivisions + (s + subdivisions)) % (stride * subdivisions)); // 3
             indices.push(i * subdivisions + (s + 1));// 1
-
+    
             indices.push(i * subdivisions + (s + 1)); // 1
             indices.push((i * subdivisions + (s + subdivisions)) % (stride * subdivisions));// 3
             indices.push((i * subdivisions + (s + subdivisions+1)) % (stride * subdivisions));// 4
         }
     }
-    
-
-    // Create flat triangle fan caps to seal the top and bottom.
-    //~ createCylinderCap(true);
-    //~ createCylinderCap(false);
 
     BABYLON.VertexData.ComputeNormals(positions, indices, normals);
     // Result
@@ -140,28 +60,10 @@ var createPath = function (diameterTop, diameterBottom, tessellation, points, sc
     vertexData.indices = indices;
     vertexData.positions = positions;
     vertexData.normals = normals;
+    console.log(normals);
     vertexData.uvs = uvs;
 
     return vertexData;
-};
-
-
-function displayVector (vector, color, scene) {
-    if (typeof(color) === "string") {
-        switch (color) {
-            case "red" : color = new BABYLON.Color3(1, 0, 0);
-            break;
-            case "green" : color = new BABYLON.Color3(0, 1, 0);
-            break;
-            case "blue" : color = new BABYLON.Color3(0, 0, 1);
-        }
-    }
-    
-    var B = vector.add(vector.scale(1/2));
-    var A = B.subtract(vector);
-
-    var line = BABYLON.Mesh.CreateLines("line", [A, B], scene);
-    line.color = color;
 };
 
 function displayAxis (center, direction, color, scene) {
@@ -176,8 +78,107 @@ function displayAxis (center, direction, color, scene) {
     }
     
     var O = center;
-    var A = direction.subtract(center);
+    var A = center.add(direction);
 
     var line = BABYLON.Mesh.CreateLines("line", [O, A], scene);
     line.color = color;
+};
+
+function frenetFrames (points) {
+
+    var	tangent = new BABYLON.Vector3(),
+        normal = new BABYLON.Vector3(),
+        binormal = new BABYLON.Vector3();
+    var frames = {},
+    tangents = frames.tangents = [];
+    normals = frames.normals = [];
+    binormals = frames.binormals = [];
+
+    var vec = new BABYLON.Vector3(),
+    mat = new BABYLON.Matrix(),
+
+    theta,
+    epsilon = 0.0001,
+    smallest,
+
+    tx, ty, tz,
+    i, u, v;
+
+    // compute the tangent vectors for each segment on the path
+    for ( i = 0; i < points.length; i ++ ) {
+        tangents[i] = getTangent(points, i);
+        BABYLON.Vector3.Normalize(tangents[i]);
+    }
+
+    function getTangent (points, it) {
+        var tangent;
+        if (it == 0) {
+            tangent = points[it+1].subtract(points[it]);
+        }
+        else if (it == points.length - 1) {
+            tangent = points[it].subtract(points[it-1]);
+        }
+        else {
+            tangent = points[it+1].subtract(points[it-1]);
+        }
+        return BABYLON.Vector3.Normalize(tangent);
+    };
+    initialNormal3();
+
+
+    function initialNormal3() {
+        // select an initial normal vector perpenicular to the first tangent vector,
+        // and in the direction of the smallest tangent xyz component
+
+        normals[ 0 ] = new BABYLON.Vector3();
+        binormals[ 0 ] = new BABYLON.Vector3();
+        smallest = Number.MAX_VALUE;
+        tx = Math.abs( tangents[ 0 ].x );
+        ty = Math.abs( tangents[ 0 ].y );
+        tz = Math.abs( tangents[ 0 ].z );
+
+        if ( tx <= smallest ) {
+            smallest = tx;
+            normal = new BABYLON.Vector3( 1, 0, 0 );
+        }
+
+        if ( ty <= smallest ) {
+            smallest = ty;
+            normal = new BABYLON.Vector3( 0, 1, 0 );
+        }
+
+        if ( tz <= smallest ) {
+            normal = new BABYLON.Vector3( 0, 0, 1 );
+        }
+
+        vec = BABYLON.Vector3.Normalize(BABYLON.Vector3.Cross( tangents[0], normal ));
+
+        normals[ 0 ] = BABYLON.Vector3.Cross( tangents[ 0 ], vec );
+        binormals[ 0 ] = BABYLON.Vector3.Cross( tangents[ 0 ], normals[ 0 ] );
+    }
+
+
+    // compute the slowly-varying normal and binormal vectors for each segment on the path
+    for ( i = 1; i < points.length; i ++ ) {
+
+        normals[ i ] = normals[ i-1 ].clone();
+        binormals[ i ] = binormals[ i-1 ].clone();
+        vec = BABYLON.Vector3.Cross( tangents[ i-1 ], tangents[ i ] );
+
+        if ( vec.length() > epsilon ) {
+
+            BABYLON.Vector3.Normalize(vec);
+            console.log(tangents[i],tangents[i-1]);
+            theta = Math.acos(BABYLON.Vector3.Dot(tangents[i-1], tangents[i]));
+
+            var rotationMatrix = BABYLON.Matrix.RotationAxis(vec, theta);
+            BABYLON.Vector3.TransformNormalToRef(normals[i], rotationMatrix, normals[i]);
+
+        }
+
+        binormals[ i ] = BABYLON.Vector3.Cross( tangents[ i ], normals[ i ] );
+
+    }
+    console.log(frames);
+    return frames;
 };
